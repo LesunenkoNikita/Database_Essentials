ccc28115d2d13f5eb177d5fc7e10b1426e92782d
LesunenkoNikita <151050806+LesunenkoNikita@users.noreply.github.com>
2024-03-29 20:33:50 +0200

    Update pa3.sql

--- pa3.sql
+++ pa3.sql
@@ -244,7 +244,7 @@ WHERE b.genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction');
-# Обрали книги жанром яких є фантастика, SELECT = with non-correlated subqueries result
+-- Обрали книги жанром яких є фантастика, SELECT = with non-correlated subqueries result
 
 SELECT *
 FROM books b
@@ -252,14 +252,14 @@ WHERE b.genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
-# Обрали книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, SELECT IN with non-correlated subqueries result
+-- Обрали книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, SELECT IN with non-correlated subqueries result
 SELECT *
 FROM books b
 WHERE b.genre_id NOT IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
-# Обрали книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, SELECT NOT IN with non-correlated subqueries result
+-- Обрали книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, SELECT NOT IN with non-correlated subqueries result
 
 SELECT *
 FROM books b
@@ -268,7 +268,7 @@ WHERE EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction');
 
-# Обрати книги, в яких жанр фантастика, EXISTS with non-correlated subqueries result
+-- Обрати книги, в яких жанр фантастика, EXISTS with non-correlated subqueries result
 
 SELECT *
 FROM books b
@@ -277,7 +277,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction');
 
-# Обрати всі книги, окрім тих, в яких жанр фантастика, NOT EXISTS with non-correlated subqueries result
+-- Обрати всі книги, окрім тих, в яких жанр фантастика, NOT EXISTS with non-correlated subqueries result
 
 UPDATE books
 SET price = 19.99
@@ -285,7 +285,7 @@ WHERE genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction');
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with = with non-correlated subqueries result
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with = with non-correlated subqueries result
 
 UPDATE books
 SET price = 19.99
@@ -293,7 +293,7 @@ WHERE genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, наукової фантастики або містики, update query with IN with non-correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, наукової фантастики або містики, update query with IN with non-correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -302,7 +302,7 @@ WHERE genre_id NOT IN (
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
 
-# змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, наукової фантастики або містики, update query with  NOT IN with non-correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, наукової фантастики або містики, update query with  NOT IN with non-correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -310,7 +310,7 @@ WHERE EXISTS (
               SELECT id
               FROM genres
               WHERE genres.name = 'Fiction');
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with EXISTS with non-correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with EXISTS with non-correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -319,7 +319,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction');
 
-# змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, update query with NOT EXISTS with non-correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, update query with NOT EXISTS with non-correlated subqueries
 
 DELETE
 FROM books b
@@ -327,7 +327,7 @@ WHERE b.genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction');
-# Видалити книги жанром яких є фантастика, DELETE = with non-correlated subqueries result
+-- Видалити книги жанром яких є фантастика, DELETE = with non-correlated subqueries result
 
 DELETE
 FROM books b
@@ -335,7 +335,7 @@ WHERE b.genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
-# видалити книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, DELETE IN with non-correlated subqueries result
+-- видалити книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, DELETE IN with non-correlated subqueries result
 
 DELETE
 FROM books b
@@ -343,7 +343,7 @@ WHERE b.genre_id NOT IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction'));
-# Видалити книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, DELETE NOT IN with non-correlated subqueries result
+-- Видалити книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, DELETE NOT IN with non-correlated subqueries result
 
 DELETE
 FROM books b
@@ -352,7 +352,7 @@ WHERE EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction');
 
-# Видалити книги, в яких жанр фантастика,DELETE EXISTS with non-correlated subqueries result
+-- Видалити книги, в яких жанр фантастика,DELETE EXISTS with non-correlated subqueries result
 
 DELETE
 FROM books b
@@ -361,7 +361,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction');
 
-# Видалити всі книги, окрім тих, в яких жанр фантастика, DELETE NOT EXISTS with non-correlated subqueries result
+-- Видалити всі книги, окрім тих, в яких жанр фантастика, DELETE NOT EXISTS with non-correlated subqueries result
 
 SELECT * 
 FROM books b
@@ -369,7 +369,7 @@ WHERE genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction' AND genres.id = b.genre_id);
-# Обрали книги жанром яких є фантастика, SELECT = with correlated subqueries result
+-- Обрали книги жанром яких є фантастика, SELECT = with correlated subqueries result
 
 SELECT *
 FROM books b
@@ -377,14 +377,14 @@ WHERE b.genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction') AND genres.id = b.genre_id);
-# Обрали книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, SELECT IN with correlated subqueries result
+-- Обрали книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, SELECT IN with correlated subqueries result
 SELECT *
 FROM books b
 WHERE b.genre_id NOT IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction') AND genres.id = b.genre_id);
-# Обрали книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, SELECT NOT IN with correlated subqueries result
+-- Обрали книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, SELECT NOT IN with correlated subqueries result
 
 SELECT *
 FROM books b
@@ -393,7 +393,7 @@ WHERE EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
 
-# Обрати книги, в яких жанр фантастика, EXISTS with correlated subqueries result
+-- Обрати книги, в яких жанр фантастика, EXISTS with correlated subqueries result
 
 SELECT *
 FROM books b
@@ -402,7 +402,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
 
-# Обрати всі книги, окрім тих, в яких жанр фантастика, NOT EXISTS with correlated subqueries result
+-- Обрати всі книги, окрім тих, в яких жанр фантастика, NOT EXISTS with correlated subqueries result
 
 UPDATE books
 SET price = 19.99
@@ -410,7 +410,7 @@ WHERE genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction' AND genres.id = b.genre_id);
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with = with correlated subqueries result
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with = with correlated subqueries result
 
 UPDATE books
 SET price = 19.99
@@ -418,7 +418,7 @@ WHERE genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction') AND genres.id = b.genre_id);
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, наукової фантастики або містики, update query with IN with correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, наукової фантастики або містики, update query with IN with correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -427,7 +427,7 @@ WHERE genre_id NOT IN (
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction') AND genres.id = b.genre_id);
 
-# змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, наукової фантастики або містики, update query with  NOT IN with correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, наукової фантастики або містики, update query with  NOT IN with correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -435,7 +435,7 @@ WHERE EXISTS (
               SELECT id
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
-# змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with EXISTS with correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок жанру фантастики, update query with EXISTS with correlated subqueries
 
 UPDATE books
 SET price = 19.99
@@ -444,7 +444,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
 
-# змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, update query with NOT EXISTS with correlated subqueries
+-- змінити ціну на 19.99 у всіх книжок будь-якого жанру, крім фантастики, update query with NOT EXISTS with correlated subqueries
 
 DELETE
 FROM books b
@@ -452,7 +452,7 @@ WHERE b.genre_id = (
                     SELECT id
                     FROM genres
                     WHERE name = 'Fiction' AND genres.id = b.genre_id);
-# Видалити книги жанром яких є фантастика, DELETE = with correlated subqueries result
+-- Видалити книги жанром яких є фантастика, DELETE = with correlated subqueries result
 
 DELETE
 FROM books b
@@ -460,7 +460,7 @@ WHERE b.genre_id IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction') AND genres.id = b.genre_id);
-# видалити книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, DELETE IN with correlated subqueries result
+-- видалити книги жанром яких є один з жанрів : фантастика, наукова фантастика, містика, DELETE IN with correlated subqueries result
 
 DELETE
 FROM books b
@@ -468,7 +468,7 @@ WHERE b.genre_id NOT IN (
                      SELECT id
                      FROM genres
                      WHERE name IN ('Fiction', 'Mystery', 'Science Fiction' AND genres.id = b.genre_id);
-# Видалити книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, DELETE NOT IN with correlated subqueries result
+-- Видалити книги жанром яких не є один з жанрів : фантастика, наукова фантастика, містика, DELETE NOT IN with correlated subqueries result
 
 DELETE
 FROM books b
@@ -477,7 +477,7 @@ WHERE EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
 
-# Видалити книги, в яких жанр фантастика,DELETE EXISTS with correlated subqueries result
+-- Видалити книги, в яких жанр фантастика,DELETE EXISTS with correlated subqueries result
 
 DELETE
 FROM books b
@@ -486,7 +486,7 @@ WHERE NOT EXISTS (
               FROM genres
               WHERE genres.name = 'Fiction' AND genres.id = b.genre_id);
 
-# Видалити всі книги, окрім тих, в яких жанр фантастика, DELETE NOT EXISTS with correlated subqueries result
+-- Видалити всі книги, окрім тих, в яких жанр фантастика, DELETE NOT EXISTS with correlated subqueries result
 
 
 
