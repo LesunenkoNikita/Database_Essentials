cb9db918da3acbdfeed686c26262c8a0f68b3d22
LesunenkoNikita <151050806+LesunenkoNikita@users.noreply.github.com>
2024-04-16 23:26:33 +0300

    Update and rename pa2.sql to queries_pa2.sql

---
+++ queries_pa2.sql
@@ -0,0 +1,15 @@
+SELECT b.title, g.name
+FROM books b
+JOIN genres_mtm gm ON b.id = gm.book_id
+JOIN genres g ON gm.genre_id = g.id
+WHERE gm.genre_id = 1;
+
+SELECT c.name, l.loan_date
+FROM loans l
+JOIN customers_mtm cm ON l.id = cm.loan_id
+JOIN customers c ON c.id = cm.customer_id
+WHERE cm.customer_id = 1
+
+
+
+
